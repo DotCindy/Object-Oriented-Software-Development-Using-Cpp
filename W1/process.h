43e1abab920364b8dbc3d94259dd388d1cc8b8b3
Cindy Le <cle15@myseneca.ca>
2018-10-02 23:24:48 -0400

    Create process.h

---
+++ W1/process.h
@@ -0,0 +1,13 @@
+/*
+Name:	Cindy Le
+Email:	cle15@myseneca.ca
+Date:	Sep 13, 2018
+OOP345 - Workshop 1
+*/
+
+#ifndef PROCESS_H
+#define PROCESS_H
+
+void process(const char* str, std::ostream& os);
+
+#endif
