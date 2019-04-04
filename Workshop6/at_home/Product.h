0078927817247843e08a246bebc809ff060240c8
Cindy Le <cle15@myseneca.ca>
2019-04-04 12:22:43 -0400

    Create Product.h

---
+++ Workshop6/at_home/Product.h
@@ -0,0 +1,49 @@
+/*
+Name:   Cindy Le
+Email:  cle15@myseneca.ca
+Date:   Mar 6, 2019
+OOP345 - Workshop 6
+*/
+
+#ifndef SICT_PRODUCT_H
+#define SICT_PRODUCT_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+
+namespace sict {
+	class iProduct {
+	public:
+		virtual double price() const = 0;
+		virtual void display(std::ostream& os) const = 0;
+	};
+
+	std::ostream& operator<<(std::ostream& os, const iProduct& p);
+	iProduct* readRecord(std::ifstream& file);
+
+	class Product : public iProduct {
+		double m_price;
+		std::string m_productNo;
+		Product() {};
+	public:
+		Product(std::string& product, double price);
+		double price() const;
+		void display(std::ostream& os) const;
+	};
+
+	class TaxableProduct : public Product {
+		const double m_HST = 1.13;
+		const double m_PST = 1.08;
+		char m_taxCode;
+		TaxableProduct();
+	public:
+		TaxableProduct(std::string& product, double price, char taxCode);
+		double price() const;
+		void display(std::ostream& os) const;
+	};
+}
+
+#endif
