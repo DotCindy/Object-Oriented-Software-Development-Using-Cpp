223f435712df0d4b9f7260e40ee41597eaf54313
Cindy Le <cle15@myseneca.ca>
2019-04-04 11:28:56 -0400

    Create w1.cpp

---
+++ Workshop1/at_home/w1.cpp
@@ -0,0 +1,29 @@
+/*
+Name:	Cindy Le
+Email:	cle15@myseneca.ca
+Date:	Jan 17, 2018
+OOP345 - Workshop 1
+*/
+
+#include <iostream>
+#include "process.h"
+
+int main(int argc, char* argv[]) {
+	std::cout << "Command Line : ";
+	for (int i = 0; i < argc; i++) {
+		std::cout << argv[i] << ' ';
+	}
+
+	if (argc < 2) {
+		std::cout << "***Insufficient number of arguments***" << std::endl;
+		return 1;
+	}
+
+	std::cout << std::endl;
+
+	for (int i = 1; i < argc; i++) {
+		sict::process(argv[i]);
+	}
+
+	return 0;
+}
